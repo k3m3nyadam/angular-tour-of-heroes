a6f0b3d6d901c152ed1fbd967d06bc6a80a1dd47
k3m3nyadam <121235430+k3m3nyadam@users.noreply.github.com>
2023-01-17 18:04:42 +0100

    Main files

---
+++ src/app/_components/index.ts
@@ -0,0 +1,4 @@
+export * from './hero-detail/hero-detail.component';
+export * from './heroes/heroes.component';
+export * from './messages/messages.component';
+export * from './dashboard/dashboard.component';
---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HeroDetailComponent, HeroesComponent, DashboardComponent } from './_components';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'heroes', component: HeroesComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'detail/:id', component: HeroDetailComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
